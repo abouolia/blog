4ac4660f9ab73325273d690a01f304854ca009bd
a.bouhuolia <a.bouhuolia@live.com>
2022-05-02 11:52:32 +0200

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
       </Head>
 
       <GlobalStyles />
