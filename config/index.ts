a4b308862a33689cda342a79cf50aae201cd873c
Ahmed Bouhuolia <a.bouhuolia@gmail.com>
2023-12-30 23:51:26 +0200

    feat: update content

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,6 @@ export const config = {
   siteTitle: 'Ahmed Bouhuolia',
   navbarRoutes: [
     { route: '/posts', title: 'Posts' },
-    { route: '/notes', title: 'Notes' },
   ],
   navbarAvatar: '/images/avatar.png',
 };
